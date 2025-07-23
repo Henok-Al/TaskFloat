12762e65dd100c910b0049bdfd0c98bc5011b0a3
Henok-Al <henok.gebresenbet@gmail.com>
2025-07-23 17:27:52 +0300

    dashboard page added

---
+++ client/src/components/index.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+import { Chart } from "./Chart";
+import Loading from "./Loading";
+import ModalWrapper from "./ModalWrapper";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import UserAvatar from "./UserAvatar";
+import UserInfo from "./UserInfo";
+
+export {
+  Button,
+  Chart,
+  Loading,
+  ModalWrapper,
+  Navbar,
+  Sidebar,
+  UserAvatar,
+  UserInfo,
+};

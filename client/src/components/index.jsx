588007e40cfe266c1c7564cb8180c2fe331e4095
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-06 15:26:12 +0300

    add task form

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -9,17 +9,22 @@ import Title from "./Title";
 import UserInfo from "./UserInfo";
 import ChangePassword from "./ChangePassword";
 import Table from "./Table";
+import ConfirmatioDialog, { UserAction } from "./ConfirmationDialog";
+import Tabs from "./Tabs";
+
 
 export {
   Button,
   Chart,
   Loading,
   ModalWrapper,
+  ConfirmatioDialog,
   Navbar,
   Sidebar,
   ChangePassword,
   UserAvatar,
   UserInfo,
   Title,
-  Table
+  Table,
+  Tabs
 };
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -37,19 +37,19 @@ const Tasks = () => {
   }, [open]);
 
   return isLoading ? (
-    <div className="py-10">
+    <div className='py-10'>
       <Loading />
     </div>
   ) : (
-    <div className="w-full">
-      <div className="flex items-center justify-between mb-4">
+    <div className='w-full'>
+      <div className='flex items-center justify-between mb-4'>
         <Title title={status ? `${status} Tasks` : "Tasks"} />
 
         {!status && user?.isAdmin && (
           <Button
-            label="Create Task"
-            icon={<IoMdAdd className="text-lg" />}
-            className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5"
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
             onClick={() => setOpen(true)}
           />
         )}
@@ -58,13 +58,13 @@ const Tasks = () => {
       <div>
         <Tabs tabs={TABS} setSelected={setSelected}>
           {!status && (
-            <div className="w-full flex justify-between gap-4 md:gap-x-12 py-4">
-              <TaskTitle label="To Do" className={TASK_TYPE.todo} />
+            <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+              <TaskTitle label='To Do' className={TASK_TYPE.todo} />
               <TaskTitle
-                label="In Progress"
+                label='In Progress'
                 className={TASK_TYPE["in progress"]}
               />
-              <TaskTitle label="Completed" className={TASK_TYPE.completed} />
+              <TaskTitle label='Completed' className={TASK_TYPE.completed} />
             </div>
           )}
 

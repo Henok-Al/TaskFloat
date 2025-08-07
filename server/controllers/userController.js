2684f58154ad3aa932f5cf3eddd4eae925859757
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-07 13:36:34 +0300

    register user

---
+++ server/controllers/userController.js
@@ -0,0 +1,38 @@
+import asyncHandler from "express-async-handler"
+import User from "../models/userModel.js";
+
+// POST - Register a new user
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, isAdmin, role, title } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Email address already exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    isAdmin,
+    role,
+    title,
+  });
+
+  if (user) {
+    isAdmin ? createJWT(res, user._id) : null;
+
+    user.password = undefined;
+
+    res.status(201).json(user);
+  } else {
+    return res
+      .status(400)
+      .json({ status: false, message: "Invalid user data" });
+  }
+});
+
+export { registerUser };
--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 import express from "express"
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import morgan from "morgan";
 import dbConnection from "./utils/connectDB.js";
+import routes from "./routes/index.js";
+
 dotenv.config();
 
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+//app.use(morgan("dev"));
+app.use("/api", routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

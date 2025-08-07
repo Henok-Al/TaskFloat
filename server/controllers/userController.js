e17f896665afaad3cd1c49da48503cd857cae0d3
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-07 14:31:55 +0300

    get team list users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -79,5 +79,26 @@ const logoutUser = (req, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 };
 
+// GET - Get list of users
+const getTeamList = asyncHandler(async (req, res) => {
+  const { search } = req.query;
+  let query = {};
 
-export { registerUser, loginUser, logoutUser };
+  if (search) {
+    const searchQuery = {
+      $or: [
+        { title: { $regex: search, $options: "i" } },
+        { name: { $regex: search, $options: "i" } },
+        { role: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ],
+    };
+    query = { ...query, ...searchQuery };
+  }
+
+  const user = await User.find(query).select("name title role email isActive");
+
+  res.status(201).json(user);
+});
+
+export { registerUser, loginUser,logoutUser,getTeamList };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,14 @@
 import express from  "express"
-import { loginUser, logoutUser, registerUser } from "../controllers/userController.js"
+import { loginUser, logoutUser, registerUser,getTeamList } from "../controllers/userController.js"
+import { isAdminRoute,protectRoute } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser);
+
+router.get("/get-team",protectRoute,isAdminRoute,getTeamList)
 
 
 export default router

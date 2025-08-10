29307d63bf916ac0f6e1a9229ac763bcadb7b8ed
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-10 14:12:49 +0300

    implment duplicate task , post task activity

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -71,7 +71,89 @@ const createTask = asyncHandler(async (req, res) => {
   }
 });
 
+const duplicateTask = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.user;
 
+    const task = await Task.findById(id);
+
+    //alert users of the task
+    let text = "New task has been assigned to you";
+    if (team.team?.length > 1) {
+      text = text + ` and ${task.team?.length - 1} others.`;
+    }
+
+    text =
+      text +
+      ` The task priority is set a ${
+        task.priority
+      } priority, so check and act accordingly. The task date is ${new Date(
+        task.date
+      ).toDateString()}. Thank you!!!`;
+
+    const activity = {
+      type: "assigned",
+      activity: text,
+      by: userId,
+    };
+
+    const newTask = await Task.create({
+      ...task,
+      title: "Duplicate - " + task.title,
+    });
+
+    newTask.team = task.team;
+    newTask.subTasks = task.subTasks;
+    newTask.assets = task.assets;
+    newTask.links = task.links;
+    newTask.priority = task.priority;
+    newTask.stage = task.stage;
+    newTask.activities = activity;
+    newTask.description = task.description;
+
+    await newTask.save();
+
+    await Notice.create({
+      team: newTask.team,
+      text,
+      task: newTask._id,
+    });
+
+    res
+      .status(200)
+      .json({ status: true, message: "Task duplicated successfully." });
+  } catch (error) {
+    return res.status(500).json({ status: false, message: error.message });
+  }
+});
+
+const postTaskActivity = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { userId } = req.user;
+  const { type, activity } = req.body;
+
+  try {
+    const task = await Task.findById(id);
+
+    const data = {
+      type,
+      activity,
+      by: userId,
+    };
+    task.activities.push(data);
+
+    await task.save();
+
+    res
+      .status(200)
+      .json({ status: true, message: "Activity posted successfully." });
+  } catch (error) {
+    return res.status(400).json({ status: false, message: error.message });
+  }
+});
 export {
-    createTask
+    createTask,
+    duplicateTask,
+    postTaskActivity
 }
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { isAdminRoute, protectRoute } from "../middleware/authMiddleware.js";
-import { createTask } from "../controllers/taskController.js";
+import { createTask, duplicateTask, postTaskActivity } from "../controllers/taskController.js";
 
 
 const router = express.Router()
 
 router.post("/create", protectRoute, isAdminRoute, createTask);
+router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
+router.post("/activity/:id", protectRoute, postTaskActivity);
 
 export default router;

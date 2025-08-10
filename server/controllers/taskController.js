7e7b82661a09077b1b5b72397e9b47fcd5b36394
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-10 14:19:10 +0300

    implement dashboard,get single task and get tasks

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -152,8 +152,148 @@ const postTaskActivity = asyncHandler(async (req, res) => {
     return res.status(400).json({ status: false, message: error.message });
   }
 });
+
+const dashboardStatistics = asyncHandler(async (req, res) => {
+  try {
+    const { userId, isAdmin } = req.user;
+
+    // Fetch all tasks from the database
+    const allTasks = isAdmin
+      ? await Task.find({
+          isTrashed: false,
+        })
+          .populate({
+            path: "team",
+            select: "name role title email",
+          })
+          .sort({ _id: -1 })
+      : await Task.find({
+          isTrashed: false,
+          team: { $all: [userId] },
+        })
+          .populate({
+            path: "team",
+            select: "name role title email",
+          })
+          .sort({ _id: -1 });
+
+    const users = await User.find({ isActive: true })
+      .select("name title role isActive createdAt")
+      .limit(10)
+      .sort({ _id: -1 });
+
+    // Group tasks by stage and calculate counts
+    const groupedTasks = allTasks?.reduce((result, task) => {
+      const stage = task.stage;
+
+      if (!result[stage]) {
+        result[stage] = 1;
+      } else {
+        result[stage] += 1;
+      }
+
+      return result;
+    }, {});
+
+    const graphData = Object.entries(
+      allTasks?.reduce((result, task) => {
+        const { priority } = task;
+        result[priority] = (result[priority] || 0) + 1;
+        return result;
+      }, {})
+    ).map(([name, total]) => ({ name, total }));
+
+    // Calculate total tasks
+    const totalTasks = allTasks.length;
+    const last10Task = allTasks?.slice(0, 10);
+
+    // Combine results into a summary object
+    const summary = {
+      totalTasks,
+      last10Task,
+      users: isAdmin ? users : [],
+      tasks: groupedTasks,
+      graphData,
+    };
+
+    res
+      .status(200)
+      .json({ status: true, ...summary, message: "Successfully." });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ status: false, message: error.message });
+  }
+});
+
+const getTask = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const task = await Task.findById(id)
+      .populate({
+        path: "team",
+        select: "name title role email",
+      })
+      .populate({
+        path: "activities.by",
+        select: "name",
+      })
+      .sort({ _id: -1 });
+
+    res.status(200).json({
+      status: true,
+      task,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch task", error);
+  }
+});
+
+const getTasks = asyncHandler(async (req, res) => {
+  const { userId, isAdmin } = req.user;
+  const { stage, isTrashed, search } = req.query;
+
+  let query = { isTrashed: isTrashed ? true : false };
+
+  if (!isAdmin) {
+    query.team = { $all: [userId] };
+  }
+  if (stage) {
+    query.stage = stage;
+  }
+
+  if (search) {
+    const searchQuery = {
+      $or: [
+        { title: { $regex: search, $options: "i" } },
+        { stage: { $regex: search, $options: "i" } },
+        { priority: { $regex: search, $options: "i" } },
+      ],
+    };
+    query = { ...query, ...searchQuery };
+  }
+
+  let queryResult = Task.find(query)
+    .populate({
+      path: "team",
+      select: "name title email",
+    })
+    .sort({ _id: -1 });
+
+  const tasks = await queryResult;
+
+  res.status(200).json({
+    status: true,
+    tasks,
+  });
+});
+
 export {
     createTask,
     duplicateTask,
-    postTaskActivity
+    postTaskActivity,
+    dashboardStatistics,
+    getTask,
+    getTasks,
 }
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAdminRoute, protectRoute } from "../middleware/authMiddleware.js";
-import { createTask, duplicateTask, postTaskActivity } from "../controllers/taskController.js";
+import { createTask, dashboardStatistics, duplicateTask, postTaskActivity,getTask,getTasks } from "../controllers/taskController.js";
 
 
 const router = express.Router()
@@ -8,5 +8,9 @@ const router = express.Router()
 router.post("/create", protectRoute, isAdminRoute, createTask);
 router.post("/duplicate/:id", protectRoute, isAdminRoute, duplicateTask);
 router.post("/activity/:id", protectRoute, postTaskActivity);
+
+router.get("/dashboard", protectRoute, dashboardStatistics);
+router.get("/", protectRoute, getTasks);
+router.get("/:id", protectRoute, getTask);
 
 export default router;

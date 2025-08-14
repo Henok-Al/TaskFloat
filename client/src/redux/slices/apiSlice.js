aed94d5092bde547b59d4bff6618d76204a51a7a
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-14 18:24:41 +0300

    some changes

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_URI = "http://localhost:8800/api";
+// const API_URI = "http://localhost:8800/api";
+const API_URI = "https://taskfloat.onrender.com/api";
+
 
 const baseQuery = fetchBaseQuery({ baseUrl: API_URI });
 

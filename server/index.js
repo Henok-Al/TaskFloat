3687473d6135a37e0ca55b4a82c711d220260b02
Henok Gebresenbet <henokgebresenbet@Henoks-MacBook-Pro.local>
2025-08-14 14:49:38 +0300

    implement task trash, delete task

--- server/index.js
+++ server/index.js
@@ -1,16 +1,27 @@
-import express from "express"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
 import morgan from "morgan";
-import dbConnection from "./utils/connectDB.js";
+import { errorHandler, routeNotFound } from "./middleware/errorMiddleware.js";
 import routes from "./routes/index.js";
+import dbConnection from "./utils/connectDB.js";
 
 dotenv.config();
 
+dbConnection();
 
+const port = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +31,7 @@ app.use(cookieParser());
 //app.use(morgan("dev"));
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    dbConnection();
-})
+app.use(routeNotFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
